8be32564a09631bc0c807c765d8fea05468d79c5
CLeeBenjamin <castonboyd@gmail.com>
2023-10-24 10:32:39 -0400

    react-app setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      hey frontend for budget App
+    </div>
+  );
+}
+
+export default App;
